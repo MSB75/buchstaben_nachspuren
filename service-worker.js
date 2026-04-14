c1c44cd6b1e1884c76b120c5b8c6578d0e2a12b0
MSB75 <m_scheibelhut@web.de>
2026-04-14 10:38:18 +0200

    Update cache version to nachspuren-v3

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker – Buchstaben nachspuren
 // Version 2 – GitHub Pages + iPad kompatibel
 
-const CACHE_VERSION = 'nachspuren-v2';
+const CACHE_VERSION = 'nachspuren-v3';
 
 // Kern-Dateien – müssen immer verfügbar sein
 const CORE_FILES = [
